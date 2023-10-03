335ed125fb0567f6f2d20f968b8cbd8ff29739b8
root <root@DESKTOP-J4MPJLL>
2023-10-03 21:31:59 +0100

    update

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+# Scan and print matching text for each pattern
+puts ARGV[0].scan(/hbttn/).join
+puts ARGV[0].scan(/hbtttn/).join
+puts ARGV[0].scan(/hbttttn/).join
+puts ARGV[0].scan(/hbtttttn/).joinn
