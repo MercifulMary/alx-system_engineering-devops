abcd544dde02ce4a3b5d62e44e0406f4d09aacb0
root <root@DESKTOP-J4MPJLL>
2023-10-03 21:05:13 +0100

    update

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,16 +1,3 @@
 #!/usr/bin/env ruby
 
-# Check if there is a command-line argument provided
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <text>"
-  exit 1
-end
-
-# Iterate through the command-line arguments
-ARGV.each do |arg|
-  # Use a regular expression to match the word "School" in the argument
-  if arg.match(/School/)
-    # Print the matched text
-    puts arg
-  end
-end
+puts ARGV[0].scan(/[S]chool/).join
