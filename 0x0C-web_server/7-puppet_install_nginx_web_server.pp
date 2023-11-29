defbae08acba08eaa4eadf1c814d2f11a3c0baf2
MercifulMary <marybakare90@gmail.com>
2023-11-29 01:00:47 +0100

    Advance tasks

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,39 @@
-#This pupppet scripts assure an nginx installation
+# puppet manifest to configure nginx
 
-package {'nginx':
-ensure => installed,,
-name   => 'nginx',
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
+}
+
+# Configure NGINX listening on port 80 and serving Hello World! on root
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/default.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Enable redirect configuration
+file { '/etc/nginx/sites-available/redirect':
+  ensure  => file,
+  content => template('nginx/redirect.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+file { '/etc/nginx/sites-enabled/redirect':
+  ensure => link,
+  target => '/etc/nginx/sites-available/redirect',
+  require => File['/etc/nginx/sites-available/redirect'],
+  notify => Service['nginx'],
 }
 
 service { 'nginx':
-ensure     => running,
-enable     => true,
-hasrestart => true,
-require    => Package['nginx'],
-subscribe  => File_line["add redirect"],
-}
-
-file {'/etc/nginx/html/index.html':
-ensure  =>  present,
-path    => '/etc/nginx/html/index.html',
-content => 'Hello World!\n',
-}
-
-file_line {'add redirect':
-ensure  => present,
-path    => '/etc/nginx/sites-available/default',
-replace => true,
-line    => '	rewrite /redirect_me/ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-match   => '# pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000',
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
 }
--- 0x0C-web_server/README.md
+++ 0x0C-web_server/README.md
@@ -1,3 +1,3 @@
-0x0C. Web serve
-
+## Web Server
+This project is about setting up my domain name
 -y on apt-get commandr
