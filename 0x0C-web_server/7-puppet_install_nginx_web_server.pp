d717a673c847e575d841dfe38d9e1c223db3c32b
MercifulMary <marybakare90@gmail.com>
2023-11-29 02:32:30 +0100

    task5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,39 +1,75 @@
 # puppet manifest to configure nginx
 
-# Install Nginx package
-package { 'nginx':
-  ensure => installed,
+# install nginx
+exec {'cmd':
+  command => 'sudo apt-get update && sudo apt-get install -y nginx',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
 
-# Configure NGINX listening on port 80 and serving Hello World! on root
+# change folder rights
+exec { 'chmod www folder':
+  command => 'chmod -R 755 /var/www',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# create index file
 file { '/var/www/html/index.html':
-  content => 'Hello World!',
+  content => "Hello World!\n",
 }
 
+# create index file
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page\n",
+}
+
+# add redirection and error page
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+  path    => '/etc/nginx/sites-available/default',
+  content =>
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        location / {
+                try_files \$uri \$uri/ =404;
+        }
+        error_page 404 /404.html;
+        location  /404.html {
+            internal;
+        }
+        
+        if (\$request_filename ~ redirect_me){
+            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+        }
+}
+",
+  require => Exec['cmd'],
 }
 
-# Enable redirect configuration
-file { '/etc/nginx/sites-available/redirect':
-  ensure  => file,
-  content => template('nginx/redirect.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+exec {'restart service':
+  command => 'sudo service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
+  require => Exec['cmd'],
 }
 
-file { '/etc/nginx/sites-enabled/redirect':
-  ensure => link,
-  target => '/etc/nginx/sites-available/redirect',
-  require => File['/etc/nginx/sites-available/redirect'],
-  notify => Service['nginx'],
+exec {'sym link':
+  command => 'ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/',
+  require => File['/etc/nginx/sites-available/default'],
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
+# restart nginx
+#exec { 'restart service':
+#  command => 'service nginx restart',
+#  path    => '/usr/bin:/usr/sbin:/bin',
+#}
 
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
-}
+# start service nginx
+#service { 'nginx':
+#  ensure     => running,
+#  enable     => true,
+# hasrestart => true,
+# require    => Package['nginx'],
+#}
